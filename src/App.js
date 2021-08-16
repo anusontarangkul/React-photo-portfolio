f70f960fb895a16e21e6cf25c1faf22f694faf55
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-16 01:49:51 -0700

    add about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
